628e777840f3947c898df4216a484061ccb0d9f3
ClaudioDumont <claudio@chaordicsystems.com>
2019-02-15 19:36:42 -0200

    Create a structure, app component, list component, add some behaviour to list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from "./hero";
+
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Wolverine'},
+    {id: 12, name: 'Magneto'},
+    {id: 13, name: 'Batman'},
+    {id: 14, name: 'Superman'},
+    {id: 15, name: 'Gavião Arqueiro'},
+    {id: 16, name: 'Lanterna Verde'},
+    {id: 17, name: 'Hulk'},
+    {id: 18, name: 'Thor'},
+    {id: 19, name: 'Homem Aranha'},
+    {id: 20, name: 'Ciclope'},
+    {id: 21, name: 'Professor Xavier'},
+    {id: 22, name: 'Homem de Ferro'}
+]
